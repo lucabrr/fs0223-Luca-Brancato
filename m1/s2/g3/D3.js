bde522f7c0f290ca680911267f59c69b18cce450
lucabrr <lucabrancato95@gmail.com>
2023-04-05 16:55:08 +0200

    fix

--- m1/s2/g3/D3.js
+++ m1/s2/g3/D3.js
@@ -217,16 +217,18 @@ if(numero % 2 == 0 ){
 }
 
 /* ESERCIZIO 10
-  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
-  let val = 7
-  if (val < 10) {
+  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.*/
+
+  let val = 3
+
+  if (val >= 5 && val < 10) {
       console.log("Meno di 10");
     } else if (val < 5) {
       console.log("Meno di 5");
     } else {
       console.log("Uguale a 10 o maggiore");
     }
-*/
+
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
