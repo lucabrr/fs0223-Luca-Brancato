94ff0687e847cca27e185bfa327de0b0926c8589
lucabrr <lucabrancato95@gmail.com>
2023-04-05 17:22:35 +0200

    finiti esercizi

--- m1/s2/g3/D3.js
+++ m1/s2/g3/D3.js
@@ -235,13 +235,17 @@ if(numero % 2 == 0 ){
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
 */
-
+{
 const me = {
   name: 'John',
   lastName: 'Doe',
   skills: ['javascript', 'html', 'css'],
 }
 
+me.city = "Toronto"
+
+console.log(me);
+}
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 12
@@ -249,21 +253,68 @@ const me = {
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+{
+  const me = {
+    name: 'John',
+    lastName: 'Doe',
+    skills: ['javascript', 'html', 'css'],
+  }
+  
+  me.city = "Toronto"
+  
+  
+  delete me.lastName;
+
+  console.log(me);
+
+  }
 
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+{
+const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css'],
+}
+
+me.city = "Toronto"
+
+
+delete me.lastName;
+
+//console.log(me);
+
+me.skills.pop()
+
+console.log(me);
+
+}
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+{
+  let arr = []
+  arr.push(1,2,3,4,5,6,7,8,9,10)
+
+  console.log(arr);
+}
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
-
 /* SCRIVI QUI LA TUA RISPOSTA */
+{
+  let arr = []
+  arr.push(1,2,3,4,5,6,7,8,9,10)
+
+  arr[arr.length-1 ] = 100
+
+  console.log(arr);
+}
