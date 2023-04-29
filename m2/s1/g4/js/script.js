bbb69e4fed8da27cbcf90767721cc8db834024df
lucabrr <lucabrancato95@gmail.com>
2023-04-29 18:07:05 +0200

    resa migliore animazione navbar post consegna

--- m2/s1/g4/js/script.js
+++ m2/s1/g4/js/script.js
@@ -1,16 +1,21 @@
 let navBar = document.querySelector("nav")
 let button = document.querySelector("nav button")
-console.log(button);
+
 
 window.addEventListener("scroll", () => {
-    if (document.documentElement.scrollTop > 400) {
+    if (window.scrollY> 400) {
+      navBar.classList.remove("animated2")
+      button.classList.remove("buttonAnimation2")
       navBar.classList.add("animated")
       button.classList.add("buttonAnimation")
-    } else if(document.documentElement.scrollTop < 400){
+      
+      
+    } else if(window.scrollY < 400 && navBar.classList == "animated" ){
         navBar.classList.remove("animated")
-        button.classList.remove("buttonAnimation");}
-        // else if(document.documentElement.scrollTop < 400 && "animated" in navBar.classList == false){
-        //     navBar.classList.add("animated2")
-        // }
-        console.log(navBar.classList.value == "animated") ;
+        button.classList.remove("buttonAnimation");
+        navBar.classList.add("animated2")
+        button.classList.add("buttonAnimation2")
+      }
+       
+        
   });
