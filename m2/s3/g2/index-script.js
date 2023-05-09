dfabbfeeff9e080f8c4919cfaa7c387faa040b37
lucabrr <lucabrancato95@gmail.com>
2023-05-09 16:47:58 +0200

    fine ex

---
+++ m2/s3/g2/index-script.js
@@ -0,0 +1,39 @@
+let form = document.querySelector("#formName")
+let nameInput = document.querySelector("#nome")
+let removeButton = document.querySelector("#removeButton")
+let saveButton = document.querySelector("#saveButton")
+let previousName = document.querySelector("#previousName")
+
+form.addEventListener("submit",(e) => {
+e.preventDefault()
+})
+
+saveButton.addEventListener("click",()=>{
+    localStorage.setItem("nome",nameInput.value)
+    nameInput.value = ""
+    previousName.textContent = `nome precedente : ${localStorage.getItem("nome")}`
+
+})
+removeButton.addEventListener("click",(e)=>{
+    localStorage.removeItem("name")
+    previousName.textContent = ""
+})
+
+
+
+let timerContainer = document.querySelector("#timer")
+let seconds = sessionStorage.getItem("tempo") || 0
+let minutsContainer = document.getElementById("minuts")
+let minuts = sessionStorage.getItem("minuts") || 0
+
+
+const timer = function(){  
+    sessionStorage.setItem("minuts",minuts)
+    sessionStorage.setItem("tempo",seconds) 
+   seconds++
+   if(seconds === 60){minuts++, seconds = 0}
+   timerContainer.textContent = seconds
+   minutsContainer.textContent = minuts
+}
+
+setInterval(timer,1000)
