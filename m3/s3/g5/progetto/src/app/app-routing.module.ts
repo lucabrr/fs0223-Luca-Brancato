5cb29e5ee8eaf92ac71129d61d66f24d634b8fa1
lucabrr <lucabrancato95@gmail.com>
2023-06-10 23:22:54 +0200

    guardia all admin

--- m3/s3/g5/progetto/src/app/app-routing.module.ts
+++ m3/s3/g5/progetto/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
+import { AuthGuard } from './Pages/authorizzation/auth.guard';
 
 const routes: Routes = [
   { path:"", component: HomeComponent}
   ,{ path: 'authorizzation', loadChildren: () => import('./Pages/authorizzation/authorizzation.module').then(m => m.AuthorizzationModule) },
-  { path: 'Admin', loadChildren: () => import('./Pages/admins-page/admins-page.module').then(m => m.AdminsPageModule) }
+  { path: 'Admin',canActivate: [AuthGuard] , loadChildren: () => import('./Pages/admins-page/admins-page.module').then(m => m.AdminsPageModule) }
 ];
 
 @NgModule({
